bd457df40b29ac1e94855c613fcb52bf05ec2c5f
AlvaroMayaCrespo <amaya@wisebuild.es>
2025-01-28 20:13:00 +0100

    filtros de reservas

--- routes/ReservaRoutes.js
+++ routes/ReservaRoutes.js
@@ -1,9 +1,9 @@
 const express = require('express');
-const { getAllReservas, createReserva, deleteReserva } = require('../controllers/ReservaController');
+const { getAllReservas, createReserva, deleteReserva, getFilter } = require('../controllers/ReservaController');
 const router = express.Router();
 
 router.get('/getAll', getAllReservas)
 router.post('/create', createReserva);
 router.post('/delete', deleteReserva);
-
+router.post('/filter', getFilter);
 module.exports = router;
