cf5b66d258d5099ba606dabe691728af8927313a
AlvaroMayaCrespo <amaya@wisebuild.es>
2025-01-29 19:59:39 +0100

    editarReservas

--- routes/ReservaRoutes.js
+++ routes/ReservaRoutes.js
@@ -1,9 +1,11 @@
 const express = require('express');
-const { getAllReservas, createReserva, deleteReserva, getFilter } = require('../controllers/ReservaController');
+const { getAllReservas, createReserva, deleteReserva, getFilter, updateReserva } = require('../controllers/ReservaController');
 const router = express.Router();
 
 router.get('/getAll', getAllReservas)
 router.post('/create', createReserva);
 router.post('/delete', deleteReserva);
 router.post('/filter', getFilter);
+router.patch('/update', updateReserva);
+
 module.exports = router;
